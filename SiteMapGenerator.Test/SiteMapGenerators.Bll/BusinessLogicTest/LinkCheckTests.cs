e6ca44e9d8b969d9dfb63357e05f8f8f4480ac6d
Vasilenko_V <vetalvasilenko@gmail.com>
2021-06-10 10:46:52 +0300

    XUnit test for class LinkCheckTests

--- SiteMapGenerator.Test/SiteMapGenerators.Bll/BusinessLogicTest/LinkCheckTests.cs
+++ SiteMapGenerator.Test/SiteMapGenerators.Bll/BusinessLogicTest/LinkCheckTests.cs
@@ -3,10 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
+using SiteMapGenerator.Bll.BusinessLogic;
+using SiteMapGenerator.Bll.BusinessLogic.Contract;
+using Xunit;
 
 namespace SiteMapGenerator.Test.SiteMapGenerators.Bll.BusinessLogicTest
 {
-    class LinkCheckTests
+    public class LinkCheckTests
     {
+        private Mock<ILinkCheck> LinkCheckUrlMock = new Mock<ILinkCheck>();
+
+        [Fact]
+        public void UrlValidation_()
+        {
+            // Arrange      
+            string url = "http://microsoft.com/";
+            var resultUrlValidation = true;
+            var linkCheck = new LinkCheck();
+
+            //  Act
+            LinkCheckUrlMock.SetupSequence(p => p.UrlValidation(url))
+                .Returns(resultUrlValidation);
+            
+            var urlResult = linkCheck.UrlValidation(url);
+
+            // Assert
+            Assert.Equal(resultUrlValidation, urlResult);
+        }
+
+        [Fact]
+        public void UrlValidation_2()
+        {
+            // Arrange      
+            string url = "microsoft.com";
+            var resultUrlValidation = false;
+            var linkCheck = new LinkCheck();
+
+            //  Act
+            LinkCheckUrlMock.SetupSequence(p => p.UrlValidation(url))
+                .Returns(resultUrlValidation);
+
+            var urlResult = linkCheck.UrlValidation(url);
+
+            // Assert
+            Assert.Equal(resultUrlValidation, urlResult);
+        }
+
+        [Fact]
+        public void AddressHost_()
+        {
+            // Arrange
+            string url = "http://microsoft.com";
+            var linkCheck = new LinkCheck();
+
+            // Act
+            LinkCheckUrlMock.SetupSequence(p => p.AddressHost(url));
+            var urlResult = linkCheck.AddressHost(url);
+
+            // Assert
+            Assert.Equal("http://microsoft.com", urlResult);
+        }
     }
 }
